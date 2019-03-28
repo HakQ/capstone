b84ad48ff80aa7761c368f9e0b73dae3cdf92f1f
Qiuqun Wang <qiuqunwang12@gmail.com>
2019-03-27 23:22:37 -0400

    pulled new updates

--- back_end/upcite_db/index.js
+++ back_end/upcite_db/index.js
@@ -5,7 +5,14 @@ const server = restify.createServer();
 
 //Middle ware
 server.use(restify.plugins.bodyParser());
-
+/*
+server.use(function crossOrigin(req,res,next) {
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Header","X-Requested-With");
+	return next();
+	}
+);
+*/
 server.listen(config.PORT, ()=>{});
 require('./routes/get_info')(server);
 console.log('Server started on port',config.PORT)
